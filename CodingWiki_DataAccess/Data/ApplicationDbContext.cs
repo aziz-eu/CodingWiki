cac6973517cdb272bf921e977acc456e2ae0ae77
abdul-aziz <abdulaziz.eu1@gmail.com>
2024-04-24 01:30:11 +0600

    connect to DB and create 1st tbl

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CodingWiki_Models.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() 
+        {
+            
+        }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring (DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=LAPTOP-B04RPVN4\\SQLEXPRESS;Database=CodingWiki;Trusted_Connection=True;TrustServerCertificate=True");
+
+        }
+    }
+}
