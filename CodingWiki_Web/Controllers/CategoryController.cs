5a94bcebdc009316b730f9079d5068a7b99f3849
abdul-aziz <abdulaziz.eu1@gmail.com>
2024-05-01 11:22:05 +0600

    Carate and Update Catagory

--- CodingWiki_Web/Controllers/CategoryController.cs
+++ CodingWiki_Web/Controllers/CategoryController.cs
@@ -18,5 +18,47 @@ namespace CodingWiki_Web.Controllers
             List<Category> categories = _db.Categories.ToList();
             return View(categories);
         }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new();
+            if ( id == 0 || id==null)
+            {
+                return View(category);
+            }
+           
+               category = _db.Categories.FirstOrDefault(u=>u.Category_Id == id);
+                if(category == null)
+                {
+                    return NotFound();
+                }
+               
+                    return View(category);
+                
+                
+            
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+         public async Task <IActionResult> Upsert(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                if(obj.Category_Id == 0)
+                {
+                    await _db.Categories.AddAsync(obj);
+                }
+                else
+                {
+                    _db.Categories.Update(obj);
+                }
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+
+        }
     }
 }
